01ac0bed4a4eb2bdaf22a6aff7b714cdc6667528
Anton Liutvinas <67786257+AntonLiutvinas@users.noreply.github.com>
2022-08-30 15:30:59 +0300

    Delete Count the smiley faces!.py

--- 6 kyu/Count the smiley faces!.py
+++
@@ -1,9 +0,0 @@
-def count_smileys(arr):
-    integer = 0
-    for i in arr:
-        if i.count(":") == 1 or i.count(";") == 1:
-            if i.count(")") or i.count("D"):
-                if len(i) == 3 and i[1] == "~" or i[1] == "-":
-                    integer += 1
-    return integer
-print(count_smileys([':D',':~)',';~D',':)']))
